99220e7fd883a19d269ef221956cf948eb9f365a
Abdullahi <abdul2kur@gmail.com>
2022-09-12 18:43:32 -0700

    100.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,12 @@ int main(void)
 		{
 			if ((j % 10) > (i % 10))
 			{
-				putchar((i % 10) + '0');
-		        	putchar((j % 10) + '0');
-			        if (i != 18 || j != 19)
-			        {
-			        	putchar(',');
-			        	putchar(' ');
+					putchar((i % 10) + '0');
+					putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+						putchar(',');
+						putchar(' ');
 				}
 			}
 
