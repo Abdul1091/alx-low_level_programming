599fab6771d54248ce4f8afb9512730b4b654478
Abdullahi <abdul2kur@gmail.com>
2022-09-13 20:29:52 -0700

    0 if false

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
  * _isalpha - function to check if c is a letter, lowercase or uppercase
  * @c: is the int that will use for the argument of the function
  * Description: check if alphabet
+ * Return: 1 if true. 0 if false
  */
 int _isalpha(int c)
 {
