7c37d90b330aacf0077d0d11097e170cefb99f6e
Abdullahi <abdul2kur@gmail.com>
2022-09-15 17:04:05 -0700

    Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ int main(void)
 		if (i == 100)
 			printf("Buzz");
 		else if (((i % 3) == 0) && ((i % 5) == 0))
-			prinf("FizzBuzz ");
+			printf("FizzBuzz ");
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
