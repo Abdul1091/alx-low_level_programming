dbb7994fe1715ab6100845f26ac9b7867e9a66ea
Abdullahi <abdul2kur@gmail.com>
2022-09-19 11:34:50 -0700

    Pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,13 @@
 /**
  * _strlen - retuens the length of a string
  * @s: string
- * Retuen: returns length as string
+ * Return: returns length as string
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*(s + len) != '\0')
 		len++;
 	return (len);
