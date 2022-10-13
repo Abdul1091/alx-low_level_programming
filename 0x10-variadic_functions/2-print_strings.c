0f87b23fa0fbe08a5c2e481406f190fba46b1aa2
Abdullahi <abdul2kur@gmail.com>
2022-10-13 14:57:18 -0700

    V-function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,7 +7,7 @@
  * @n: number of strings passed to the function
  */
 
-void print_strings(const char *separator, const unsigned int n,  ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list params;
